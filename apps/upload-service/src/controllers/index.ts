fc819404e71d78e91fef741411ce9774c482e2c5
sarthakKarode <sarthakkarode@gmail.com>
2025-09-11 22:15:52 +0530

    completed deploy-services -2

--- apps/upload-service/src/controllers/index.ts
+++ apps/upload-service/src/controllers/index.ts
@@ -15,14 +15,14 @@ export const Upload = async (req:Request, res:Response) => {
     const id = GenerateID();
     console.log("ID generated: "+id);
     console.log("Requested to clone repository");
-    await simpleGit().clone(url, `${storePath}dist/${id}`);
+    await simpleGit().clone(url, `${storePath}output/${id}`);
     console.log("Clone successfull");
-    const files = GetFilePath(`${storePath}dist/${id}`);
+    const files = GetFilePath(`${storePath}output/${id}`);
     console.log(files);
     console.log("Pushing to S3 object store initiated");
     files.forEach(async (file) => {
-        const relativePath = path.relative(`${storePath}dist/${id}`, file); 
-        await PushToS3(`${id}/${relativePath}`, file);
+        const relativePath = path.relative(`${storePath}output/${id}`, file); 
+        await PushToS3(`output/${id}/${relativePath}`, file);
     });
     RedisPublisher(id);
     console.log("Upload service completed");
@@ -30,4 +30,3 @@ export const Upload = async (req:Request, res:Response) => {
         id: id
     })
 }
-"/Users/sarthakkarode/deployNow/apps/upload-service/dist/04027/README.md"
